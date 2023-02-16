2bd48529846c8fbd29c99b39dd60d3dc54ccb2c6
Yemi <yemiadebayo60@gmail.com>
2023-02-16 13:23:49 +0000

    publish message to SNS

--- src/publishMessageDetailsToSnsHandler.ts
+++ src/publishMessageDetailsToSnsHandler.ts
@@ -1,11 +1,26 @@
+import AWS from "aws-sdk";
 import { APIGatewayEvent } from "aws-lambda";
+import { PublishInput } from "aws-sdk/clients/sns";
+import { Logger } from '@aws-lambda-powertools/logger';
+
+const logger = new Logger({ serviceName: 'publishMessageDetailsToSNS' });
+const sns = new AWS.SNS();
 
 export type HandlerEvent = Pick<APIGatewayEvent, "body">;
-export const handler = async (_event: HandlerEvent) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Lambda executed successfully!",
-      }),
-    };
+
+export const handler = async (event: HandlerEvent) => {
+  const params: PublishInput = {
+    Message:  event.body || "",
+    TopicArn: process.env.PUBLISHED_MESSAGE_DETAILS_SNS_ARN,
+  };
+
+  await sns.publish(params).promise();
+  logger.info("message published to SNS")
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Lambda executed successfully!",
+    }),
+  };
 };
--- tests/publishMessageDetailsToSnsHandler.spec.ts
+++ tests/publishMessageDetailsToSnsHandler.spec.ts
@@ -1,17 +1,38 @@
-import { handler, HandlerEvent } from "../src/publishMessageDetailsToSnsHandler";
+import AWS from "aws-sdk";
+import { handler } from "../src/publishMessageDetailsToSnsHandler";
+
+jest.mock('aws-sdk', () => {
+  const mockedSNS = {
+    publish: jest.fn().mockReturnThis(),
+    promise: jest.fn(),
+  };
+
+  return { SNS: jest.fn(() => mockedSNS) };
+});
 
 describe("publishMessageDetailsToSnsHandler", () => {
   describe("Happy path", () => {
-    it("should return a 200 response", async() => {
-      const event: HandlerEvent = {body: ""}
+    it("should publish correct params to SNS", async () => {
+      const sns = new AWS.SNS();
+      (sns.publish().promise as jest.Mock).mockResolvedValueOnce({
+        MessageId: 'some-message-id',
+      });
 
-      expect(await handler(event)).toEqual({
-          statusCode: 200,
-          body: JSON.stringify({
-            message: "Lambda executed successfully!",
-          }),
-        }
-      )
+      const event = {
+        body: JSON.stringify({
+          phoneNumber: "07865346733",
+          message: "Hello world"
+        }),
+      };
+
+      await handler(event);
+
+      const expectedParams = {
+        Message: event.body,
+        TopicArn: process.env.PUBLISHED_MESSAGE_DETAILS_SNS_ARN
+      };
+
+      expect(sns.publish).toHaveBeenCalledWith(expectedParams);
     });
   });
 });
